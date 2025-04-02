d032b63114487416531bfc64942f2a8129a1447d
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-04-02 17:11:13 +0200

    Install and test ejs

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express();
-
-app.get('/', function (req, res) {
-    res.send('Hello world!');
-})
-
 const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {name: "Julia"});
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
