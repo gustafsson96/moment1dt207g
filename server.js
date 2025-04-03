9ebdf737084500dab51a75e5e9e363462f412d19
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-04-03 20:01:56 +0200

    Create and link css file

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const connection = require('./install');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded( {extended: true}));
 
 app.get('/', (req, res) => {
     res.render('index');
