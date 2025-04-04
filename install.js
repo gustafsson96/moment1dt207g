30de9a229f2358a4a8d0f89d6f07a60d48cd9c25
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-04-04 18:15:10 +0200

    Redo the install.js file

--- install.js
+++ install.js
@@ -1,6 +1,7 @@
 const { Client } = require("pg");
 require("dotenv").config();
 
+// Connect to database
 const client = new Client({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
@@ -8,27 +9,27 @@ const client = new Client({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     ssl: {
-        rejectUnauthorized: false
+        rejectUnauthorized: false,
     }
 });
 
-
 client.connect((err) => {
     if (err) {
-        console.log ("Fel vid anslutning: " + err);
+        console.log("Fel vid anslutning" + err);
     } else {
         console.log("Ansluten till databasen...")
     }
 });
 
-//Create table
+// Create course table
 client.query(`
-    CREATE TABLE IF NOT EXISTS courses (
+    DROP TABLE IF EXISTS courses;
+    CREATE TABLE courses(
         id SERIAL PRIMARY KEY,
         course_code VARCHAR(20) NOT NULL UNIQUE,
         course_name VARCHAR(200) NOT NULL,
         course_progression VARCHAR(10) NOT NULL,
         course_syllabus VARCHAR(500),
         course_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )
+        )
     `);
